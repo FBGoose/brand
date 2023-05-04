ad2bcd33afab257aeff167496f4c12a4bc1f01dd
FBGoose <116726697+FBGoose@users.noreply.github.com>
2023-05-04 20:12:10 +0800

    Update BrandServiceImpl.java

--- brand/src/main/java/com/itheima/service/impl/BrandServiceImpl.java
+++ brand/src/main/java/com/itheima/service/impl/BrandServiceImpl.java
@@ -58,7 +58,7 @@ public class BrandServiceImpl implements BrandService {
         System.out.println("1111111111");
         System.out.println("1111111111111");
         System.out.println("1111111222222222222222222222222222222");
-        System.out.println("111");
+        System.out.println("11122222");
     }
 
     @Override
